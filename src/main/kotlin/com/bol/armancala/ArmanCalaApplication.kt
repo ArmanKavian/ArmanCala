b276656a85975c133eb120d522059984e6345f75
Arman.Kavian <arman.kavian@cloudflight.io>
2023-11-15 03:47:34 +0100

    Initial commit.

---
+++ src/main/kotlin/com/bol/armancala/ArmanCalaApplication.kt
@@ -0,0 +1,11 @@
+package com.bol.armancala
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArmanCalaApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArmanCalaApplication>(*args)
+}
---
+++ src/test/kotlin/com/bol/armancala/ArmanCalaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bol.armancala
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArmanCalaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
